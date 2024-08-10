8b74f4d19e740c41e3318d820230a00f0ac84640
Ramla Eman <ramla.eman.ra@gmail.com>
2024-08-10 19:02:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Question 1:Declare 3 variables in one statement.
+// var myName = "Ramla Eman", myFatherName = "Rizwan Ahmed", luckyNumber = 2;
+// document.write(myName + "<br>" + myFatherName + "<br>" + luckyNumber);
+// console.log(myName + "\n" + myFatherName + "\n" + luckyNumber);
+
+// Question 2:Declare 5 legal & 5 illegal variable names. 
+// legal
+// var legal1 = "username", legal2 = "$price", legal3 = "My_Nmae", legal4 = "luckyNumber", legal5 = "_father";
+// document.write(legal1 + "<br>" + legal2 + "<br>" + legal3 + "<br>" + legal4 + "<br>" + legal5 + "<br>");
+// console.log(legal1 + "\n" + legal2 + "\n" + legal3 + "\n" + legal4 + "\n" + legal5 + "\n");
+
+// illegal
+// var illegal1 = "@tatoo", illegal2 = "1stword", illegal3 = "text mean", illegal4 = "var", illegal5 = "hindi-urdu";
+// document.write(illegal1 + "<br>" + illegal2 + "<br>" + illegal3 + "<br>" + illegal4 + "<br>" + illegal5 + "<br>");
+// console.log(illegal1 + "\n" + illegal2 + "\n" + illegal3 + "\n" + illegal4 + "\n" + illegal5 + "\n");
+
+// Question 3:Display this in your browser.
+// var heading = "Rules for naming JS variables";
+// document.write(heading + "<br>");
+// console.log(heading + "\n");
+// var a = "numbers, $ and _ ";
+// document.write(" Variable names can only contain " + a + "For example $my_1stVariable " + "<br>");
+// console.log(" Variable names can only contain " + a + "For example $my_1stVariable " + "\n");
+// var b = "letter, $ and _ ";
+// document.write("Variables must begin with a " + b + "For example $name, _name or name" + "<br>");
+// console.log("Variables must begin with a " + b + "For example $name, _name or name" + "\n");
+// var c = "sensitive";
+// document.write("Variable names are case " + c + "<br>");
+// console.log("Variable names are case " + c + "\n");
+// var d = "keywords";
+// document.write("Variable names should not be JS " + d);
+// console.log("Variable names should not be JS"  + d);
